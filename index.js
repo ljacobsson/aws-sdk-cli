1fe4fb1b4cdd58057f521c5d2af76e61942a8a9e
ljacobsson <lars@mathem.se>
2023-05-24 00:42:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import { program } from 'commander';
+import { Octokit } from '@octokit/rest';
+import * as input from "./src/inputUtil.js";
+import * as shell from 'shelljs';
+import * as fs from "fs"
+import * as os from "os"
+import * as path from "path"
+
+program
+  .version('1.0.0')
+  .description('AWS SDK v3 CLI');
+
+program
+.option('--no-cache', 'Use cached results', false)
+.action((cmd) => {
+    console.log('Search keyword:', cmd.query);
+    searchReadmeFiles(cmd.cached);
+  })
+  .parse(process.argv);
+
+
+async function searchReadmeFiles(nocache) {
+  if (!process.env.GITHUB_TOKEN) {
+    console.log("Please set GITHUB_TOKEN environment variable. Create one here https://github.com/settings/tokens");
+    process.exit(1);
+  }
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const cachePath = path.join(os.homedir(), ".aws-sdk-cli", "cache.json");
+  let choices;
+  if (fs.existsSync(cachePath) && !nocache) {
+    console.log("Using cached results. To refresh, run with --no-cache");
+    choices = JSON.parse(fs.readFileSync(cachePath).toString("utf-8"));
+  } else {
+    const response = await octokit.repos.getContent({
+      owner: 'aws',
+      repo: 'aws-sdk-js-v3',
+      path: '/clients',
+    });
+
+    const files = response.data.filter((item) => item.type === 'dir');
+
+    const fileChoices = files.map(async (file) => {
+      const readme = await octokit.repos.getContent({
+        owner: 'aws',
+        repo: 'aws-sdk-js-v3',
+        path: `/clients/${file.name}/README.md`,
+      });
+
+      const readmeContent = Buffer.from(readme.data.content, 'base64').toString();
+      return {
+        name: `@aws-sdk/${file.name}`,
+        value: { name: file.name, readme: readmeContent },
+      };
+    });
+    choices = await Promise.all(fileChoices);
+    if (!fs.existsSync(path.dirname(cachePath))) {
+      fs.mkdirSync(path.dirname(cachePath), { recursive: true });
+    }
+    fs.writeFileSync(cachePath, JSON.stringify(choices));
+  }
+  const selectedClient = await input.default.autocomplete('Search client:', choices);
+
+
+  const npmCommand = `npm install ${selectedClient.name}`;
+  console.log(`Running '${npmCommand}'`);
+  shell.default.exec(npmCommand);
+
+}
+
---
+++ src/inputUtil.js
@@ -0,0 +1,92 @@
+import * as inquirer from "inquirer";
+import * as auto from "inquirer-autocomplete-prompt";
+
+const inq = inquirer.default;
+
+inq.registerPrompt(
+  "autocomplete",
+  auto.default
+);
+
+async function choices(message, items, type, defaults, pageSize = 5) {
+  return (
+    await inq.prompt({
+      type: type,
+      name: "item",
+      choices: items,
+      message: message,
+      default: defaults,
+      pageSize: pageSize,
+      source: function (answersYet, input) {
+        if (!input) {
+          return items;
+        }
+
+        const split = input.split(" ");
+        return items.filter(
+          (p) =>
+            !p.value ||
+            split.filter((f) => p.value.readme.toLowerCase().includes(f.toLowerCase()))
+              .length === split.length
+        );
+      },
+    })
+  ).item;
+}
+
+async function files(message) {
+  return (
+    await inq.default.prompt({
+      type: "file-tree-selection",
+      name: "files",
+      message: message,
+      multiple: true
+    })
+  ).files;
+}
+
+async function text(message, defaultValue) {
+  return (
+    await inq.default.promptprompt({
+      type: "input",
+      name: "text",
+      default: defaultValue,
+      message: message,
+    })
+  ).text;
+}
+async function autocomplete(message, items) {
+  return await choices(message, items, "autocomplete", null, 7);
+}
+
+async function list(message, items) {
+  return await choices(message, items, "list", null);
+}
+
+async function checkbox(message, items, defaults) {
+  let list = [];
+  do {
+    list = await choices(message, items, "checkbox", defaults);
+  } while (list.length === 0);
+  return list;
+}
+
+async function prompt(message) {
+  return (
+    await inq.default.promptprompt({
+      type: "confirm",
+      name: "choice",
+      default: "Yes",
+      message: message,
+    })
+  ).choice;
+}
+
+export default {
+  autocomplete,
+  list,
+  checkbox,
+  text,
+  prompt,
+  files,
+};
